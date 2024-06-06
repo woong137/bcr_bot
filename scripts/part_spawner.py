62701ac6694ebce341c0f97b5acfc8c971a087a9
woong137 <woong137137@gmail.com>
2024-06-06 19:52:58 +0900

    feat: Update part spawner script to include distance calculation for deletion

--- scripts/part_spawner.py
+++ scripts/part_spawner.py
@@ -14,6 +14,10 @@ class PartSpawner():
         self.rospack = rospkg.RosPack()
         self.path = self.rospack.get_path('bcr_bot')+"/models/"
         self.part = self.path+"car_wheel/model.sdf"
+        self.part_name = "car_wheel"
+        self.spawn_point = Point(x=0, y=0, z=0.3)
+        self.spawn_angle = [0, 0, 0]  # rad
+        self.delete_point = Point(x=1, y=1, z=0)
 
         self.spawn_model = rospy.ServiceProxy(
             "/gazebo/spawn_sdf_model", SpawnModel)
@@ -23,26 +27,34 @@ class PartSpawner():
             "/gazebo/get_model_state", GetModelState)
 
     def checkModel(self):
-        res = self.model_state("part", "world")
+        res = self.model_state(self.part_name, "world")
         return res.success
 
     def getPosition(self):
-        res = self.model_state("part", "world")
+        res = self.model_state(self.part_name, "world")
         return res.pose.position.z
+
+    def getDistance(self):
+        res = self.model_state(self.part_name, "world")
+        return ((self.delete_point.x - res.pose.position.x)**2 +
+                (self.delete_point.y - res.pose.position.y)**2 +
+                (self.delete_point.z - res.pose.position.z)**2)\
+            ** 0.5
 
     def spawnModel(self):
         part = self.part
         with open(part, "r") as f:
             part_sdf = f.read()
 
-        quat = tf.transformations.quaternion_from_euler(0, 0, 0)
+        quat = tf.transformations.quaternion_from_euler(
+            self.spawn_angle[0], self.spawn_angle[1], self.spawn_angle[2])
         orient = Quaternion(quat[0], quat[1], quat[2], quat[3])
-        pose = Pose(Point(x=0, y=0, z=0.3), orient)
-        self.spawn_model("part", part_sdf, '', pose, 'world')
+        pose = Pose(self.spawn_point, orient)
+        self.spawn_model(self.part_name, part_sdf, '', pose, 'world')
         rospy.sleep(1)
 
     def deleteModel(self):
-        self.delete_model("part")
+        self.delete_model(self.part_name)
         rospy.sleep(1)
 
     def shutdown_hook(self):
@@ -62,6 +74,6 @@ if __name__ == "__main__":
     while not rospy.is_shutdown():
         if part_spawner.checkModel() == False:
             part_spawner.spawnModel()
-        elif part_spawner.getPosition() < 0.05:
+        elif part_spawner.getDistance() < 0.5:
             part_spawner.deleteModel()
         rate.sleep()
