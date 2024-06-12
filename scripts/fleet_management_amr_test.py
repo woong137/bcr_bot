6413897ca56ba87c7d0989f3d9499acbf2abf5a8
woong137 <woong137137@gmail.com>
2024-06-12 16:55:09 +0900

    Refactor fleet management AMR script to improve part spawning logic

--- scripts/fleet_management_amr_test.py
+++ scripts/fleet_management_amr_test.py
@@ -19,7 +19,8 @@ class FleetManagerAMR:
             "bcr_bot_4": ["S5", "D5"],
         }
         self.current_zones = {bot: 0 for bot in self.target_zones.keys()}
-        self.publishers = {bot_name: rospy.Publisher(f"/{bot_name}/target_zone", String, queue_size=10) for bot_name in self.target_zones.keys()}
+        self.publishers = {bot_name: rospy.Publisher(
+            f"/{bot_name}/target_zone", String, queue_size=10) for bot_name in self.target_zones.keys()}
 
     def publish_target_zone(self, bot_name, target_zone):
         self.publishers[bot_name].publish(target_zone)
@@ -29,7 +30,8 @@ class FleetManagerAMR:
             current_index = self.current_zones[robot_namespace]
             target_zone = zones[current_index]
             target_zone_value = part_spawner.zones[target_zone]
-            has_model = part_spawner.check_model(target_zone_value['part'], robot_namespace)
+            has_model = part_spawner.check_model(
+                target_zone_value['part'], robot_namespace)
 
             if current_index == 0 and has_model:
                 self.current_zones[robot_namespace] = 1
@@ -49,32 +51,45 @@ class PartSpawner:
     def __init__(self):
         self.rospack = rospkg.RosPack()
         self.path = self.rospack.get_path('bcr_bot') + "/models/"
-        self.zones = self.load_zone_coordinates(self.rospack.get_path('bcr_bot') + "/param/zone_coordinates.yaml")
+        self.zones = self.load_zone_coordinates(
+            self.rospack.get_path('bcr_bot') + "/param/zone_coordinates.yaml")
         self.distance_threshold = 0.11
         self.angle_threshold = 0.1
-        self.robots = ["bcr_bot_0", "bcr_bot_1", "bcr_bot_2", "bcr_bot_3", "bcr_bot_4"]
+        self.robots = ["bcr_bot_0", "bcr_bot_1",
+                       "bcr_bot_2", "bcr_bot_3", "bcr_bot_4"]
 
-        self.spawn_model = rospy.ServiceProxy("/gazebo/spawn_sdf_model", SpawnModel)
-        self.delete_model = rospy.ServiceProxy("/gazebo/delete_model", DeleteModel)
-        self.model_state = rospy.ServiceProxy("/gazebo/get_model_state", GetModelState)
+        self.spawn_model = rospy.ServiceProxy(
+            "/gazebo/spawn_sdf_model", SpawnModel)
+        self.delete_model = rospy.ServiceProxy(
+            "/gazebo/delete_model", DeleteModel)
+        self.model_state = rospy.ServiceProxy(
+            "/gazebo/get_model_state", GetModelState)
 
     def main(self, fleet_manager_amr):
         for robot_namespace in self.robots:
-            target_zone = fleet_manager_amr.target_zones[robot_namespace][fleet_manager_amr.current_zones[robot_namespace]]
+            target_zone = fleet_manager_amr.target_zones[robot_namespace][
+                fleet_manager_amr.current_zones[robot_namespace]]
             target_zone_value = self.zones[target_zone]
-            is_at_zone = self.check_robot_reached_zone(robot_namespace, target_zone_value)
+            is_at_zone = self.check_robot_reached_zone(
+                robot_namespace, target_zone_value)
             is_supply_zone = target_zone.startswith('S')
             is_demand_zone = target_zone.startswith('D')
-            has_model = self.check_model(target_zone_value['part'], robot_namespace)
+            has_model = self.check_model(
+                target_zone_value['part'], robot_namespace)
 
             if not has_model and is_at_zone and is_supply_zone:
-                rospy.loginfo(f"{robot_namespace} 로봇이 {target_zone} 구역에서 부품 {target_zone_value['part']}를 수령했습니다.")
-                spawn_point = Point(x=target_zone_value['x'], y=target_zone_value['y'], z=0.5)
-                self.spawn_model_func(target_zone_value['part'], robot_namespace, spawn_point, [0, 0, 0])
+                rospy.loginfo(
+                    f"{robot_namespace} 로봇이 {target_zone} 구역에서 부품 {target_zone_value['part']}를 수령했습니다.")
+                spawn_point = Point(
+                    x=target_zone_value['x'], y=target_zone_value['y'], z=0.5)
+                self.spawn_model_func(
+                    target_zone_value['part'], robot_namespace, spawn_point, [0, 0, 0])
 
             elif has_model and is_at_zone and is_demand_zone:
-                rospy.loginfo(f"{robot_namespace} 로봇이 {target_zone} 구역에서 부품 {target_zone_value['part']}를 공급했습니다.")
-                self.delete_model_func(target_zone_value['part'], robot_namespace)
+                rospy.loginfo(
+                    f"{robot_namespace} 로봇이 {target_zone} 구역에서 부품 {target_zone_value['part']}를 공급했습니다.")
+                self.delete_model_func(
+                    target_zone_value['part'], robot_namespace)
 
     def check_model(self, part, robot_namespace):
         part_name = f"{part}({robot_namespace})"
@@ -89,14 +104,16 @@ class PartSpawner:
         trans, euler = self.get_pose(robot_namespace)
         if trans is None or euler is None:
             return False
-        distance = ((zone['x'] - trans[0]) ** 2 + (zone['y'] - trans[1]) ** 2) ** 0.5
+        distance = ((zone['x'] - trans[0]) ** 2 +
+                    (zone['y'] - trans[1]) ** 2) ** 0.5
         angle = abs(zone['theta'] - euler[2])
         return distance < self.distance_threshold and angle < self.angle_threshold
 
     def spawn_model_func(self, part, robot_namespace, spawn_point, spawn_angle):
         with open(self.path + part + "/model.sdf", "r") as f:
             part_sdf = f.read()
-        quat = tf.transformations.quaternion_from_euler(spawn_angle[0], spawn_angle[1], spawn_angle[2])
+        quat = tf.transformations.quaternion_from_euler(
+            spawn_angle[0], spawn_angle[1], spawn_angle[2])
         orient = Quaternion(quat[0], quat[1], quat[2], quat[3])
         pose = Pose(spawn_point, orient)
         part_name = f"{part}({robot_namespace})"
@@ -116,8 +133,10 @@ class PartSpawner:
     def get_pose(self, robot_namespace):
         listener = tf.TransformListener()
         try:
-            listener.waitForTransform('map', f'{robot_namespace}/base_footprint', rospy.Time(), rospy.Duration(10.0))
-            (trans, rot) = listener.lookupTransform('map', f'{robot_namespace}/base_footprint', rospy.Time(0))
+            listener.waitForTransform(
+                'map', f'{robot_namespace}/base_footprint', rospy.Time(), rospy.Duration(10.0))
+            (trans, rot) = listener.lookupTransform(
+                'map', f'{robot_namespace}/base_footprint', rospy.Time(0))
             euler = tf.transformations.euler_from_quaternion(rot)
             return trans, euler
         except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
@@ -150,6 +169,14 @@ if __name__ == "__main__":
 
     rate = rospy.Rate(1)
     while not rospy.is_shutdown():
+        target_zones = {
+            "bcr_bot_0": ["S1", "D1"],
+            "bcr_bot_1": ["S2", "D2"],
+            "bcr_bot_2": ["S3", "D3"],
+            "bcr_bot_3": ["S4", "D4"],
+            "bcr_bot_4": ["S5", "D5"],
+        }
+        robots = list(target_zones.keys())
         fleet_manager_amr.main(part_spawner)
         part_spawner.main(fleet_manager_amr)
         rate.sleep()
